c61ac4c52bca82a8c3ef8ccf17e62c4270ae916e
Luciferian Ink <LuciferianInk@protonmail.com>
2023-10-16 02:17:15 -0500

    add basic GUN chat

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # fvn
 
 F is for friends
+```
+Swarm state: healthy
 
-## Features
+...2vYiga, 256.0 RPS  |                                 ########                             |
+...vMfXzi, 3688.5 RPS |#########################                                             |
+...i9CA9T, 2776.2 RPS |                              ###############                         |
+...33Pgb9, 2108.6 RPS |###############                                                       |
+...Ap3RUq, 256.0 RPS  |                                     ########                         |
+...7mhsNH, 256.0 RPS  |                         ########                                     |
+...iRUz6M, 2767.3 RPS |               ###############                                        |
+...zbDrjo, 3500.3 RPS |                                             #########################|
+
+
+Legend:
+
+# - online
+J - joining     (loading blocks)
+? - unreachable (port forwarding/NAT/firewall issues, see below)
+_ - offline     (just disconnected)
+```
+
+## features
 
 Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
 
--- extension.js
+++ extension.js
@@ -3,11 +3,14 @@
 const vscode = require("vscode")
 const http = require("http")
 const Gun = require("gun")
+// const SEA = require("gun/sea")
 // import * as http from 'http'
 // import Gun from 'gun'
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+let gun = null
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -22,14 +25,15 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fvn.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('fvn.pong', function () {
 		// The code you place here will be executed every time your command is executed
-
+		sendMessage(gun)
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fvn!');
+		vscode.window.showInformationMessage('Pong!');
 	});
 
 	context.subscriptions.push(disposable);
+
 }
 
 // This method is called when your extension is deactivated
@@ -48,7 +52,7 @@ async function relayGUN() {
 			console.log(`server listening on port: 60666`)
 		})
 
-	const gun = Gun({
+	gun = Gun({
 		peers: ["https://59.src.eco/gun", "https://95.src.eco/gun"],
 		web: server,
 		file: './gun',
@@ -56,6 +60,55 @@ async function relayGUN() {
 		radisk: false,
 		axe: false
 	})
+
+	logMessages(gun)
+}
+
+function sendMessage(gun) {
+	gun.get('src').get('bullets').get('trade').put(JSON.stringify({
+		focus: 'trade',
+		message: 'pong',
+		identifier: 'randomId',
+		mode: 'cos'
+	}))
+	console.log(`${bc.CORE}ONE@FVN:${ad.TEXT} pong`)
+}
+
+function logMessages(gun) {
+	gun.get('src').get('bullets').get('trade').on(async (data) => {
+		const bullet = JSON.parse(data)
+		let message = ''
+		let color = bc.CORE
+		if (
+			bullet.pubKey !== null &&
+			typeof bullet.pubKey !== 'undefined'
+		) {
+			const sender = await gun.user(bullet.pubKey)
+			if (typeof sender === 'undefined') {
+				message = bullet.message
+				color = bc.FOLD
+			} else
+				message = await Gun.SEA.verify(
+					bullet.message,
+					sender.pub
+				)
+				color = bc.ROOT
+		} else {
+			message = bullet.message
+			color = bc.FOLD
+		}
+		console.log(`${color}ONE@FVN:${ad.TEXT} ${message}`)
+	})
+}
+
+const bc = {
+    FOLD: '\x1b[34m',
+    ROOT: '\x1b[32m',
+    CORE: '\x1b[31m'
+}
+
+const ad = {
+    TEXT: '\x1b[0m'
 }
 
 module.exports = {
