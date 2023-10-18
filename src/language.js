f285b690b13b1b52add30349aba682f573f4cefb
Luciferian Ink <LuciferianInk@protonmail.com>
2023-10-17 20:16:56 -0500

    color sort of works

--- src/language.js
+++ src/language.js
@@ -1,6 +1,13 @@
 const vscode = require('vscode')
-const tokenTypes = ['class', 'interface', 'enum', 'function', 'variable']
-const tokenModifiers = ['declaration', 'documentation']
+const tokenTypes = [
+    'class',
+    'interface',
+    'enum',
+    'function',
+    'variable',
+    'keyword'
+]
+const tokenModifiers = ['declaration', 'documentation', 'input']
 const legend = new vscode.SemanticTokensLegend(tokenTypes, tokenModifiers)
 
 const provider = {
@@ -9,19 +16,21 @@ const provider = {
 
         const tokensBuilder = new vscode.SemanticTokensBuilder(legend)
         // on line 1, characters 1-5 are a class declaration
+
         tokensBuilder.push(
             new vscode.Range(
-                new vscode.Position(1, 1),
-                new vscode.Position(1, 5)
+                new vscode.Position(1, 0),
+                new vscode.Position(1, 11)
             ),
-            'class',
-            ['declaration']
+            'keyword',
+            ['input']
         )
+
         return tokensBuilder.build()
     }
 }
 
-const selector = { language: 'src', scheme: 'file' } // register for all Java documents from the local file system
+const selector = { language: 'src' } // register for all Java documents from the local file system
 
 vscode.languages.registerDocumentSemanticTokensProvider(
     selector,
